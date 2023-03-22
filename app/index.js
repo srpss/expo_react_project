1d301972761f16fbfdd17a53350602638632c97b
srpss <61586478+srpss@users.noreply.github.com>
2023-03-22 17:55:00 +0200

    welcome page many changes

--- app/index.js
+++ app/index.js
@@ -1,14 +1,39 @@
-import { useState  } from 'react';
-import {View, Text,  ScrollView, SafeAreaView} from 'react-native';
-import {Stack,useRouter} from 'expo-router'
+import { useState } from 'react';
+import { View, Text, ScrollView, SafeAreaView } from 'react-native';
+import { Stack, useRouter } from 'expo-router'
 
-import {COLORS, icons, images, SIZES} from '../constants'
+import { COLORS, icons, images, SIZES } from '../constants'
+import { Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome } from '../components'
 
-const Home = () =>{
+
+const Home = () => {
+    const router = useRouter();;
     return (
-        <View>
-        <Text>Home test</Text>
-        </View>
+        <SafeAreaView styles={{
+            flex: 1,
+            backgroudColor: COLORS.lightWhite
+        }}>
+            <Stack.Screen
+                options={{
+                    headerStyle: { backgroundColor: COLORS.lightWhite },
+                    headerShadowVisible: false,
+                    headerLeft: () => (
+                        <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
+
+                    ),
+                    headerRight: () => (
+                        <ScreenHeaderBtn iconUrl={images.profile} dimension="100%" />
+                    ),
+                    headerTitle: ""//removes the Index at center
+                }}
+            />
+            <ScrollView showsVerticalScrollIndicator={false}>
+                <View style={{ flex: 1, padding: SIZES.medium }}><Welcome />
+                    <Popularjobs></Popularjobs>
+                    <Nearbyjobs></Nearbyjobs>
+                </View>
+            </ScrollView>
+        </SafeAreaView>
     )
 }
 
